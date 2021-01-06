03016c0d5f68cf449fdd0674d6d5b662c05759f6
Kilson <kilsonrs@gmail.com>
2021-01-06 17:24:51 -0300

    create a instances of UpdateUserUseCases

---
+++ src/useCases/UpdateUser/index.ts
@@ -0,0 +1,9 @@
+import PostgresUserRepository from '../../repositories/implementations/PostgresUserRepository';
+import UpdateUserController from './UpdateUserController';
+import UpdateUserUseCase from './UpdateUserUseCase';
+
+const userRepository = new PostgresUserRepository();
+const updateUserUseCase = new UpdateUserUseCase(userRepository);
+const updateUserController = new UpdateUserController(updateUserUseCase);
+
+export { updateUserUseCase, updateUserController };
