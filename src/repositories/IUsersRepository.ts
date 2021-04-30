83e6d1b9de1a7f9349102ebbbfd8c6a2c4d07d1b
Kilson <kilsonrs@gmail.com>
2021-04-30 15:52:11 -0300

    refactor: remove undefined return type

--- src/repositories/IUsersRepository.ts
+++ src/repositories/IUsersRepository.ts
@@ -3,9 +3,9 @@ import ICreateUserDTO from '../useCases/CreateUser/CreateUserDTO';
 import IUpdateUserDTO from '../useCases/UpdateUser/UpdateUserDTO';
 
 interface IUsersRepository {
-  findById(id: string): Promise<User | undefined>;
-  findByCpf(cpf: string): Promise<User | undefined>;
-  findByEmail(email: string): Promise<User | undefined>;
+  findById(id: string): Promise<User>;
+  findByCpf(cpf: string): Promise<User>;
+  findByEmail(email: string): Promise<User>;
   create(user: ICreateUserDTO): Promise<User>;
   save(user: IUpdateUserDTO): Promise<User>;
 }
--- src/repositories/implementations/PostgresUserRepository.ts
+++ src/repositories/implementations/PostgresUserRepository.ts
@@ -1,4 +1,4 @@
-import { getRepository } from 'typeorm';
+import { getRepository, Repository } from 'typeorm';
 
 import IUsersRepository from '../IUsersRepository';
 import User from '../../entities/User';
@@ -6,34 +6,35 @@ import ICreateUserDTO from '../../useCases/CreateUser/CreateUserDTO';
 import IUpdateUserDTO from '../../useCases/UpdateUser/UpdateUserDTO';
 
 class PostgresUserRepository implements IUsersRepository {
-  public async findById(id: string): Promise<User | undefined> {
-    const ormRepository = getRepository(User);
-    const user = await ormRepository.findOne(id);
+  private repository: Repository<User>;
+
+  constructor() {
+    this.repository = getRepository(User);
+  }
+
+  public async findById(id: string): Promise<User> {
+    const user = await this.repository.findOne(id);
     return user;
   }
 
-  public async findByCpf(cpf: string): Promise<User | undefined> {
-    const ormRepository = getRepository(User);
-    const user = await ormRepository.findOne({ where: { cpf } });
-    return user;
-  }
-
-  public async findByEmail(email: string): Promise<User | undefined> {
-    const ormRepository = getRepository(User);
-    const user = await ormRepository.findOne({ where: { email } });
+  public async findByCpf(cpf: string): Promise<User> {
+    const user = await this.repository.findOne({ where: { cpf } });
     return user;
   }
 
   public async create(userData: ICreateUserDTO): Promise<User> {
-    const ormRepository = getRepository(User);
-    const user = ormRepository.create(userData);
-    await ormRepository.save(user);
+    const user = this.repository.create(userData);
+    await this.repository.save(user);
+    return user;
+  }
+
+  public async findByEmail(email: string): Promise<User> {
+    const user = await this.repository.findOne({ email });
     return user;
   }
 
   public async save(userData: IUpdateUserDTO): Promise<User> {
-    const ormRepository = getRepository(User);
-    const user = await ormRepository.save(userData);
+    const user = await this.repository.save(userData);
     return user;
   }
 }
